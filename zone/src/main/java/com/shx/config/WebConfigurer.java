5a01a9c56bc74c0beafb6d14993ae7f10ed448ec
sunhongxin <1138778340@qq.com>
2020-06-04 17:46:18 +0800

    提交

---
+++ zone/src/main/java/com/shx/config/WebConfigurer.java
@@ -0,0 +1,30 @@
+package com.shx.config;
+
+import com.shx.intercepors.LoginInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfigurer implements WebMvcConfigurer {
+
+    @Autowired
+    private LoginInterceptor loginInterceptor;
+
+
+    //注册拦截器
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // addPathPatterns("/**") 表示拦截所有的请求，
+        // excludePathPatterns("/login", "/register") 表示除了登陆与注册之外，因为登陆注册不需要登陆也可以访问
+        registry.addInterceptor(loginInterceptor).addPathPatterns("/**").excludePathPatterns("/login","/api/login");
+    }
+
+    //配置静态资源
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+    }
+}
--- zone/src/main/java/com/shx/controller/LoginApiController.java
+++ zone/src/main/java/com/shx/controller/LoginApiController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @RestController
 @RequestMapping("/api")
 public class LoginApiController {
@@ -16,11 +19,13 @@ public class LoginApiController {
     private UserService userService;
 
     @RequestMapping("/login")
-    public Result login(@RequestBody User user){
+    public Result login(@RequestBody User user, HttpServletRequest request){
       User loginUser =   userService.login(user);
         if (loginUser != null){
+            HttpSession session = request.getSession();
+            session.setAttribute("user",loginUser);
             return new Result(true,"登录成功!");
         }
-        return new Result(false,"登录失败!");
+        return new Result(false,"登录失败!用户名或密码错误!");
     }
 }
